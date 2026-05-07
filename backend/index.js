99af7f467cec064c8ef01f527329dde2b3428eda
DJCon2005 <djconrad1219@gmail.com>
2026-05-07 09:47:00 -0500

    Fix CORS configuration and update allowedHeaders
    Updated CORS configuration and allowedHeaders property.

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,15 @@ const { connectDB } = require("./config/mongodb");
 const app = express();
 
 app.use(cors({
-  origin: ["https://localhost:5173", "https://ames-appetites-frontend.vercel.app"],
+  origin: ["http://localhost:5173", "https://ames-appetites-frontend.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowHeaders: ["Content-Type", "Authorization"]
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+app.options("*", cors({
+  origin: ["http://localhost:5173", "https://ames-appetites-frontend.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
